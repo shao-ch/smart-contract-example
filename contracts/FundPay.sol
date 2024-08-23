4c43c9585c2ee9897d9c5f6d12b89aa0dfd6ddab
shao-ch <461272202@qq.com>
2024-08-23 16:53:55 +0800

    lesson4:how to create the fund of smart contract.

---
+++ contracts/FundPay.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//get funds from users
+//withdraw dunds
+//set minimum funding value in usd;
+
+contract FundPay {
+
+    //send a funding value
+    function fund() public {
+
+        //want to set a minimum funding value in USA
+    }
+
+    function withdraw() public {
+
+    }
+    
+}
